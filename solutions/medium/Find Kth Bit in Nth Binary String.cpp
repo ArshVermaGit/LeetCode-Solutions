e8a0b0f418aeb78c5818e56fc67d2d929bdcd5fe
Arsh Verma <arshverma.dev@gmail.com>
2026-03-03 23:56:07 +0530

    Add solution for Find Kth Bit in Nth Binary String

---
+++ solutions/medium/Find Kth Bit in Nth Binary String.cpp
@@ -0,0 +1,20 @@
+// Title: Find Kth Bit in Nth Binary String
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/find-kth-bit-in-nth-binary-string/
+
+class Solution {
+public:
+    char findKthBit(int n, int k) {
+        if (n == 1) return '0';
+        
+        int len = (1 << n) - 1;
+        int mid = (len + 1) / 2;
+        
+        if (k == mid) return '1';
+        if (k < mid) return findKthBit(n - 1, k);
+        
+        char c = findKthBit(n - 1, len - k + 1);
+        return c == '0' ? '1' : '0';
+    }
+};
