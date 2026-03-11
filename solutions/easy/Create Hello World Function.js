28a5de699caf1a916b69c2218c8811909d7a6869
Arsh Verma <arshverma.dev@gmail.com>
2026-03-12 00:02:56 +0530

    Add solution for Create Hello World Function

---
+++ solutions/easy/Create Hello World Function.js
@@ -0,0 +1,10 @@
+// Title: Create Hello World Function
+            // Difficulty: Easy
+            // Language: JavaScript
+            // Link: https://leetcode.com/problems/create-hello-world-function/
+
+var createHelloWorld = function() {
+    return function(...args) {
+        return "Hello World";
+    }
+};
