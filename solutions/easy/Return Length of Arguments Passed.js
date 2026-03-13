9d80a680aaa46816f130e77cf0c5852a15240822
Arsh Verma <arshverma.dev@gmail.com>
2026-03-13 14:51:47 +0530

    Add solution for Return Length of Arguments Passed

---
+++ solutions/easy/Return Length of Arguments Passed.js
@@ -0,0 +1,8 @@
+// Title: Return Length of Arguments Passed
+            // Difficulty: Easy
+            // Language: JavaScript
+            // Link: https://leetcode.com/problems/return-length-of-arguments-passed/
+
+var argumentsLength = function(...args) {
+    return args.length;
+};
