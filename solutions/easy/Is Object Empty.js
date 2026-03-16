18585415f21b73ef997db216574d8a2f6d4e27b8
Arsh Verma <arshverma.dev@gmail.com>
2026-03-16 19:52:27 +0530

    Add solution for Is Object Empty

---
+++ solutions/easy/Is Object Empty.js
@@ -0,0 +1,8 @@
+// Title: Is Object Empty
+            // Difficulty: Easy
+            // Language: JavaScript
+            // Link: https://leetcode.com/problems/is-object-empty/
+
+var isEmpty = function(obj) {
+    return Object.keys(obj).length === 0;
+};
