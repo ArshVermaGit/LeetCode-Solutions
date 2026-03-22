b18014fdc886b0b9a4578ff50fe2f6bfb48ccc23
Arsh Verma <arshverma.dev@gmail.com>
2026-03-22 08:24:08 +0530

    Add solution for Construct Uniform Parity Array I

---
+++ solutions/easy/Construct Uniform Parity Array I.cpp
@@ -0,0 +1,5 @@
+// Title: Construct Uniform Parity Array I
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/construct-uniform-parity-array-i/
+
