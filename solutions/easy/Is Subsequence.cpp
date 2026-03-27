4b2937d769d4eb471f1a96de11f15ef22931f250
Arsh Verma <arshverma.dev@gmail.com>
2026-03-27 15:42:48 +0530

    Add solution for Is Subsequence

---
+++ solutions/easy/Is Subsequence.cpp
@@ -0,0 +1,19 @@
+// Title: Is Subsequence
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/is-subsequence/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i=0,j=0;
+        while(i<s.size() && j<t.size()){
+            if(s[i]==t[j]) i++;
+            j++;
+        }
+        return i==s.size();
+    }
+};
