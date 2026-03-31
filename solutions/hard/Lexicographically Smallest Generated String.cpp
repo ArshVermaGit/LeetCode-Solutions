b7d10ee26bde4773b7c562fafce6d5071f9a38ae
Arsh Verma <arshverma.dev@gmail.com>
2026-03-31 11:41:39 +0530

    Add solution for Lexicographically Smallest Generated String

---
+++ solutions/hard/Lexicographically Smallest Generated String.cpp
@@ -0,0 +1,38 @@
+// Title: Lexicographically Smallest Generated String
+            // Difficulty: Hard
+            // Language: C++
+            // Link: https://leetcode.com/problems/lexicographically-smallest-generated-string/
+
+
+                                if(str1[k]=='T')
+
+                                    if(string(w.
+                                        
+                                        break;
+                                    }
+                                }
+                            }
+
+                            if(valid && string
+                                fixed=true;
+                                break;
+                            }
+                        }
+
+                        if(fixed) break;
+
+                        w[pos]=original;
+                    }
+
+                    if(!fixed) return "";
+                }
+            }
+        }
+
+        return string(w.begin(),w.end());
+{
+begin()+k,w.begin()+k+m)!=str2){
+valid=false;
+(w.begin()+i,w.begin()+i+m)!=str2){
+    }
+};
