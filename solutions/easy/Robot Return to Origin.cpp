9ad7aca931a3be1f86fc5674fedc9825f8f622c7
Arsh Verma <arshverma.dev@gmail.com>
2026-04-05 13:17:56 +0530

    Add solution for Robot Return to Origin

---
+++ solutions/easy/Robot Return to Origin.cpp
@@ -0,0 +1,20 @@
+// Title: Robot Return to Origin
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/robot-return-to-origin/
+
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0, y = 0;
+        
+        for(char move : moves) {
+            if(move == 'U') y++;
+            else if(move == 'D') y--;
+            else if(move == 'R') x++;
+            else if(move == 'L') x--;
+        }
+        
+        return (x == 0 && y == 0);
+    }
+};
