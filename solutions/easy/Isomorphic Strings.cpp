dffff539d42d8596f3d9a7de93aef24a582ecd26
Arsh Verma <arshverma.dev@gmail.com>
2026-04-08 20:35:19 +0530

    Add solution for Isomorphic Strings

---
+++ solutions/easy/Isomorphic Strings.cpp
@@ -0,0 +1,27 @@
+// Title: Isomorphic Strings
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/isomorphic-strings/
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+
+        unordered_map<char,char> m1;
+        unordered_map<char,char> m2;
+
+        for(int i=0;i<s.size();i++){
+
+            if(m1.count(s[i]) && m1[s[i]]!=t[i])
+                return false;
+
+            if(m2.count(t[i]) && m2[t[i]]!=s[i])
+                return false;
+
+            m1[s[i]]=t[i];
+            m2[t[i]]=s[i];
+        }
+
+        return true;
+    }
+};
