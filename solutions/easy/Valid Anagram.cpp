d37d32713e35ef4da67cc671f223d9b46d2e3f08
Arsh Verma <arshverma.dev@gmail.com>
2026-04-10 12:18:52 +0530

    Add solution for Valid Anagram

---
+++ solutions/easy/Valid Anagram.cpp
@@ -0,0 +1,18 @@
+// Title: Valid Anagram
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/valid-anagram/
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        if(s.size() != t.size())
+            return false;
+
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        return s == t;
+    }
+};
