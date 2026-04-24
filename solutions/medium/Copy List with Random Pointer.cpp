4b76863a372aada0a0bf8dd75caf29994e970455
Arsh Verma <arshverma.dev@gmail.com>
2026-04-24 12:16:01 +0530

    Add solution for Copy List with Random Pointer

--- solutions/medium/Copy List with Random Pointer.cpp
+++ solutions/medium/Copy List with Random Pointer.cpp
@@ -3,25 +3,36 @@
             // Language: C++
             // Link: https://leetcode.com/problems/copy-list-with-random-pointer/
 
-#include <bits/stdc++.h>
-using namespace std;
-
-class Solution {
-public:
-    Node* copyRandomList(Node* head) {
         if(!head) return NULL;
-        unordered_map<Node*, Node*> mp;
+        
         Node* curr = head;
         while(curr) {
-            mp[curr] = new Node(curr->val);
-            curr = curr->next;
+            Node* next = curr->next;
+            curr->next = new Node(curr->val);
+            curr->next->next = next;
+            curr = next;
         }
+        
         curr = head;
         while(curr) {
-            mp[curr]->next = mp[curr->next];
-            mp[curr]->random = mp[curr->random];
+            if(curr->random) curr->next->random 
+            curr = curr->next->next;
+        }
+= curr->random->next;
+        
+        curr = head;
+        Node* newHead = head->next;
+        while(curr) {
+            Node* copy = curr->next;
+            curr->next = copy->next;
+            if(copy->next) copy->next = 
+copy->next->next;
             curr = curr->next;
         }
-        return mp[head];
-    }
-};
+        
+    Node* copyRandomList(Node* head) {
+public:
+class Solution {
+
+using namespace std;
+#include <bits/stdc++.h>
