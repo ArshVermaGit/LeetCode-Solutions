9d09eb015e49e68555f409ca98ad6bdc89a9744d
Arsh Verma <arshverma.dev@gmail.com>
2026-04-26 07:38:39 +0530

    Add solution for Remove Nth Node From End of List

--- solutions/medium/Remove Nth Node From End of List.cpp
+++ solutions/medium/Remove Nth Node From End of List.cpp
@@ -3,12 +3,15 @@
             // Language: C++
             // Link: https://leetcode.com/problems/remove-nth-node-from-end-of-list/
 
-        return dummy->next;
+        int pos = len - n;
+        
+        if(pos == 0) return head->next;
+        
+        temp = head;
+        for(int i = 1; i < pos; i++) temp = temp->next;
+        
+        temp->next = temp->next->next;
+        
+        return head;
     }
 };
-            fast = fast->next;
-            slow = slow->next;
-        }
-        
-        slow->next = slow->next->next;
-        
