6577a2d002d7dc3e4540ba5eef47f49a299caabf
Arsh Verma <arshverma.dev@gmail.com>
2026-04-26 07:32:13 +0530

    Add solution for Detect Cycles in 2D Grid

--- solutions/medium/Detect Cycles in 2D Grid.cpp
+++ solutions/medium/Detect Cycles in 2D Grid.cpp
@@ -3,12 +3,12 @@
             // Language: C++
             // Link: https://leetcode.com/problems/detect-cycles-in-2d-grid/
 
-        vis.assign(m, vector<int>(n, 0));
-        
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(!vis[i][j]) {
-                    if(dfs(i, j, -1, -1, grid)) return true;
+                            vis[nx][ny] = 1;
+                            q.push({nx, ny, x, y});
+                        } else if(nx != px || ny != py) {
+                            return true;
+                        }
+                    }
                 }
             }
         }
