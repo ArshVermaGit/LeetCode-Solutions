94c54a5907d869039260ba4e15ec7539fa86d990
Arsh Verma <arshverma.dev@gmail.com>
2026-04-28 07:43:11 +0530

    Add solution for Minimum Operations to Make a Uni-Value Grid

--- solutions/medium/Minimum Operations to Make a Uni-Value Grid.cpp
+++ solutions/medium/Minimum Operations to Make a Uni-Value Grid.cpp
@@ -3,11 +3,11 @@
             // Language: C++
             // Link: https://leetcode.com/problems/minimum-operations-to-make-a-uni-value-grid/
 
-        for (int val : v) {
             if (val % x != r) return -1;
         }
-        sort(v.begin(), v.end());
-        int median = v[v.size() / 2];
+        int n = v.size();
+        nth_element(v.begin(), v.begin() + n / 2, v.end());
+        int median = v[n / 2];
         int ops = 0;
         for (int val : v) {
             ops += abs(val - median) / x;
