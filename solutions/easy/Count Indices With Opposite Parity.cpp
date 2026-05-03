f33164c042e943536120c84b0d195cc9d4b2059c
Arsh Verma <arshverma.dev@gmail.com>
2026-05-03 09:29:42 +0530

    Add solution for Count Indices With Opposite Parity

---
+++ solutions/easy/Count Indices With Opposite Parity.cpp
@@ -0,0 +1,5 @@
+// Title: Count Indices With Opposite Parity
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/count-indices-with-opposite-parity/
+
