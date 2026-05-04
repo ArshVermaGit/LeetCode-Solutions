0d2e8c3896361037fc5ba08b5a7c8746929f94b3
Arsh Verma <arshverma.dev@gmail.com>
2026-05-04 19:58:21 +0530

    Add solution for Same Tree

--- solutions/easy/Same Tree.cpp
+++ solutions/easy/Same Tree.cpp
@@ -3,12 +3,15 @@
             // Language: C++
             // Link: https://leetcode.com/problems/same-tree/
 
-class Solution {
-public:
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (!p && !q) return true;
-        if (!p || !q) return false;
-        if (p->val != q->val) return false;
-        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+            TreeNode* b = sq.top(); sq.pop();
+            if (!a && !b) continue;
+            if (!a || !b) return false;
+            if (a->val != b->val) return false;
+            sp.push(a->left);
+            sq.push(b->left);
+            sp.push(a->right);
+            sq.push(b->right);
+        }
+        return sp.empty() && sq.empty();
     }
 };
